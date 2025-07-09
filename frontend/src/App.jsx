54b462553c54562da132d9e1028e4ced69e17be3
Tousif Tamboli <shaikhkashish1911@gmail.com>
2025-07-10 02:24:22 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-yellow-100 p-5 rounded text-center">
+      <h1 className="text-3xl font-bold text-yellow-800">Tailwind is working 🎉</h1>
+    </div>
+  );
+}
+
+export default App;
