3ca9d16e611752908248592cbaef9d035767b0fc
TousifTamboli <tousiftamboli3@gmail.com>
2025-07-19 22:25:40 +0530

    backend resolving

--- backend/server.js
+++ backend/server.js
@@ -27,5 +27,5 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => app.listen(5000, () => console.log("🚀 Server running on port 5000")))
-.catch(err => console.error("❌ Mongo error:", err));
+.then(() => app.listen(5000, () => console.log(" Server running on port 5000")))
+.catch(err => console.error(" Mongo error:", err));
